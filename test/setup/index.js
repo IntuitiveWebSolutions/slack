5f23a52c8dccf790ce35c6c2efc01a190351d333
Alexander Kavanaugh <alex@kavdev.io>
2020-01-07 13:24:46 -0800

    Added lint disable flags to remove test console log linting warnings.

--- test/setup/index.js
+++ test/setup/index.js
@@ -5,9 +5,9 @@ require('./nock');
 // @todo: Should create and destroy database for each test suite
 exec('./node_modules/.bin/sequelize db:migrate', (err, stdout, stderr) => {
   if (err) {
-    console.log(`stderr: ${stderr}`);
-    console.log(`stdout: ${stdout}`);
-    console.log('err', err);
+    console.log(`stderr: ${stderr}`); // eslint-disable-line no-console
+    console.log(`stdout: ${stdout}`); // eslint-disable-line no-console
+    console.log('err', err); // eslint-disable-line no-console
     // node couldn't execute the command
   }
 });
