49349f7f22edec1aa94ee2deb360855256f07424
Alexander Kavanaugh <alex@kavdev.io>
2020-01-27 11:44:21 -0800

    Added stackset config.

---
+++ sam_deploy_prod.sh
@@ -0,0 +1,9 @@
+sam build --template sam-template.yml
+aws-okta exec github-pr-channels-admin -- sam package --s3-bucket github-slack-channels --s3-prefix githubprchannels --output-template-file output-template.yml
+aws-okta exec github-pr-channels-admin -- sam deploy \
+--template-file output-template.yml \
+--stack-name GitHubSlack-LambdaStack \
+--s3-bucket github-slack-channels \
+--s3-prefix githubprchannels \
+--capabilities CAPABILITY_IAM \
+--parameter-overrides AppEnv="prod" LogLevel="warn"
